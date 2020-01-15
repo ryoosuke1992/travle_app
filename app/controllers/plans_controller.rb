d707ec8f4d2dae8dc853b88339903131599f0e1c
watanaberyosuke <ryoosuke1992@gmail.com>
2020-01-15 15:22:57 +0900

    travel_app

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,8 +1,24 @@
 class PlansController < ApplicationController
+
 	def index
-		@plan = Plan.find(1)
+		@plans = Plan.all
+	end
+
+	def show
+		@plan = Plan.find params[:id]
 	end
 
 	def new
+		@plan = Plan.new
+	end
+
+	def create
+		@plan = Plan.new(plan_params)
+		@plan.save
+		redirect_to plan_path(@plan)
+	end
+
+	def plan_params
+		params.require(:plan).permit(:title, :date, :place, :meeting_place, :meeting_time, :content)
 	end
 end
