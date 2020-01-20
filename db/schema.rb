eaee9de2be6d5e322cc13a03d97ff006c23b79b9
watanaberyosuke <ryoosuke1992@gmail.com>
2020-01-20 11:30:02 +0900

    delete-migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_18_003337) do
+ActiveRecord::Schema.define(version: 2020_01_17_131530) do
 
   create_table "plans", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
